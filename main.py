a899fdb7b1c7fa3804135a567174450d545e8975
Caleb Han <calebhantech@gmail.com>
2023-12-08 16:26:25 -0500

    commit

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+Edge: e
+Empty: 0
+Border: b
+Gate: g
+"""
+
+track = [["e00e", "ebb0", "ebgb", "ee0b"],
+         ["00be", "b000", "g0b0", "0eg0"],
+         ["bgge", "0bbg", "bg0b", "gebg"],
+         ["g0ee", "b0e0", "00e0", "bee0"]]
+
+gates_taken = []
+
